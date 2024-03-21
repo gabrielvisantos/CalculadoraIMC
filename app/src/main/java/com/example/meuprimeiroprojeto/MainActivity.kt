78a5186ee4d3c0958113f50de3c5ef96676943e4
Gabriel Vieira Santos <gabrielvieira.bio@gmail.com>
2024-03-21 11:43:34 -0300

    mostrar mensagem "campo vazio"

--- app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.meuprimeiroprojeto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +15,32 @@ class MainActivity : AppCompatActivity() {
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
-        val btnCalcular= findViewById<Button>(R.id.btn_calcular)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
-        btnCalcular.setOnClickListener{
-            val peso =  edtPeso.text
-            val altura= edtAltura.text
-            println("IMC do Gabriel" + peso)
+        btnCalcular.setOnClickListener {
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+
+                Snackbar
+                    .make(
+                    edtPeso,
+                    "Campo vazio",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                println(resultado)
+            }
+
         }
     }
 }
